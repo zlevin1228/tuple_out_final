e1c26eea46f04e75d28c5d9a1d17db5bbe429398
zlevin1228 <zacharylevin1228@gmail.com>
2026-04-27 17:07:01 -0400

    updated dice roll function and made a few other small changes. it works using multiple loops and has docstrings and the necessary testing left over.

---
+++ diceroll.py
@@ -0,0 +1,59 @@
+import numpy as np
+def dice_roll(fixed_dice=None):
+    """
+    Rolls up to 3 dice, keeping any fixed dice in place. Fixed dice are dice that have the same value as one other die. If all 3 match, the turn will be worth 0.
+
+    Parameters:
+        fixed_dice (list): a list of fixed die values to keep, or None if none need to be marked as fixed.
+
+    Returns:
+        tuple: (dice, tupled_out, turn_score)
+            - dice (list): List of 3 die values
+            - tupled_out (bool): True if all 3 dice are the same
+            - turn_score (int): Sum of dice (0 if tupled out)
+
+    # Tests
+    # result = dice_roll()
+    # print(result)        # expected: a tuple of (list of 3 ints, bool, int)
+    # result = dice_roll(fixed_dice=[5, 5, 2])
+    # print(result[0])     # expected: first two dice should still be 5 and 5
+    """
+
+    # Gets range of rollable numbers
+    numpy_numbers = np.arange(1, 7)
+
+    # Determines if there are any fix dice already, and if not it rolls all 3 fresh
+    if fixed_dice is None:
+        dice = list(np.random.choice(numpy_numbers, size = 3, replace = True))
+    else:
+        # Only rerolls dice that aren't fixed
+        dice = list(fixed_dice)
+        for i in range(3):
+            if dice[i] not in fixed_dice:
+                dice[i] = int(np.random.choice(numpy_numbers))
+
+    # Checks which dice are fixed
+    fixed_values = []
+    for die in dice:
+        if dice.count(die) > 1:
+            fixed_values.append(die)
+
+    # Display all 3 die and MARK WHICH ARE FIXED
+
+    print("\nYour dice:")
+    for i in range(3):
+        if dice[i] in fixed_values:
+            print(f"  Die {i+1}: {dice[i]}  [FIXED]")
+        else:
+            print(f"  Die {i+1}: {dice[i]}")
+
+    # Checks if the dice will tuple out
+    tupled_out = (dice[0] == dice[1] == dice[2])
+    if tupled_out:
+        print("You tupled out! You score 0 points this turn.")
+        return (dice, True, 0)
+
+    # Calculate turn score
+    turn_score = int(dice[0] + dice[1] + dice[2])
+
+    return (dice, False, turn_score)
--- tupleout.py
+++ tupleout.py
@@ -1,35 +1,36 @@
-import numpy as np
 import os 
+import numpy as np
+from diceroll import dice_roll
 # Ask the user for how many people are going to play. (Set to at max 2-3)
-player_count = input("How many players are playing? Either 2 players or 3 players can play.")
+while True:
+    try:
+        player_count = int(input("How many players are playing?\nEither 2 players or 3 players can play: "))
 
-if player_count == 2:
-    player_1 = input("What is the name for this player")
-    player_2 = input("Now what is the name for the second player")
-elif player_count == 3:
-    player_1 = input("What is the name for this player")
-    player_2 = input("Now what is the name for the second player")
-    player_3 = input("Lastly, what is the name of the third player")
-else:
-    print("Invalid input for player count. Please run the program again.") # Update later to automate
-# Code to "Radomly" generate numbers on the dies 1-6
-np
-os
+        if player_count == 2:
+            player_1 = input("What is the name for this player? ")
+            player_2 = input("Now what is the name for the second player? ")
+            break
+        elif player_count == 3:
+            player_1 = input("What is the name for this player? ")
+            player_2 = input("Now what is the name for the second player? ")
+            player_3 = input("Lastly, what is the name of the third player? ")
+            break
+        else:
+            print("Invalid input for player count. Please enter 2 or 3.") # Update later to automate
+    except ValueError:
+        print("Invalid input. Please enter a number:\n")
+# Code to "Randomly" generate numbers on the dies 1-6
+turn_score = 0
+added_score = 0
+total_score = 0
 
-numpy_numbers = np.arange(1, 4)
 
+# Code to make it "Roll Dice" (radomly generate number)
 
-#Code to make it "Roll Dice" (radomly generate number)
-
-np.random.choice(numpy_numbers, size = 3, replace = False)
-
+result = dice_roll()
 
 # Code to make it Tuple out if 3 of the same numbers are generated 
 
-if numpy_numbers[0] == numpy_numbers[1] == numpy_numbers[2]:
-    added_score = 0
-    print(f'You tupled out! Your score for this turn is: {added_score}')
-
 
 
 #
@@ -37,7 +38,7 @@ if numpy_numbers[0] == numpy_numbers[1] == numpy_numbers[2]:
 
 
 
-#
+# 
 
 
 
